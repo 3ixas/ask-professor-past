0e19124fa7c99b1b64cb32a5e3858f29fe0c9b19
Elias Bennett <eliasthebennett@gmail.com>
2025-05-06 22:49:05 +0300

    Add chat_view path to urlpatterns.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ask_professor
+from .views import ask_professor, chat_view
 
 urlpatterns = [
     path("ask/", ask_professor),
+    path("", chat_view, name="chat"),
 ]
